b9432c49d9d29119e42291896f59b7dc46cb5b04
Tom Howarth <tom.howarth@planetvm.net>
2024-02-27 14:12:49 +0000

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,9 +1,9 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 2.59.0"
-    }
-  }
-  required_version = ">= 0.13"
-}
+#terraform {
+#  required_providers {
+ #   azurerm = {
+  #    source  = "hashicorp/azurerm"
+   #   version = ">= 2.59.0"
+   # }
+  #}
+ # required_version = ">= 0.13"#
+#}
